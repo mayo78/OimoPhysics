e2b57e829aed2592d2768da7a179948c12a5e7bf
mrcdk <contact@mrcdk.com>
2023-10-09 10:40:57 +0200

    Fix error with Haxe 4.3.2

--- src/oimo/m/B.hx
+++ src/oimo/m/B.hx
@@ -1,8 +1,10 @@
 package oimo.m;
+
 import haxe.macro.Context;
 import haxe.macro.Expr;
 import haxe.macro.ExprTools;
 import haxe.macro.Type;
+
 using haxe.macro.ComplexTypeTools;
 using oimo.m.B;
 using oimo.m.U;
@@ -12,11 +14,8 @@ using Lambda;
 /**
  * Build Macro
  */
-@:extern
 class B {
-
-#if macro
-
+	#if macro
 	// print imports of all classes
 	static inline var WRITE_IMPORTS:Bool = true;
 	// print exports and externs for JS closure compiler
@@ -24,7 +23,7 @@ class B {
 	static inline var WRITE_EXTERNS:Bool = true;
 
 	static function log(s:String):Void {
-		//trace(s);
+		// trace(s);
 	}
 
 	static var onGenerateAdded:Bool = false;
@@ -39,7 +38,8 @@ class B {
 	}
 
 	static function onGenerate(types:Array<Type>):Void {
-		if (!WRITE_IMPORTS && !WRITE_EXPORTS && !WRITE_EXTERNS) return;
+		if (!WRITE_IMPORTS && !WRITE_EXPORTS && !WRITE_EXTERNS)
+			return;
 
 		var externs = [];
 		var exports = [];
@@ -47,37 +47,37 @@ class B {
 		externs.push("var OIMO;");
 		exports.push("window[\"OIMO\"] = {};");
 		types.iter((t) -> switch (t) {
-		case TInst(t, params):
-			var classType = t.get();
-			var className = classType.name;
-			var jsClassName = classType.pack.join("_") + "_" + className;
-			if (~/^oimo.(common|collision|dynamics)/.match(classType.pack.join("."))) {
-				imports.push('import ${classType.pack.join(".")}.$className;');
+			case TInst(t, params):
+				var classType = t.get();
+				var className = classType.name;
+				var jsClassName = classType.pack.join("_") + "_" + className;
+				if (~/^oimo.(common|collision|dynamics)/.match(classType.pack.join("."))) {
+					imports.push('import ${classType.pack.join(".")}.$className;');
 
-				var doc = classType.doc;
-				var hidden = classType.meta.extract(":dox").exists((me) -> me.params.exists((e) -> e.expr.equals((macro hide).expr)));
-				var isInterface = classType.meta.has(":interface");
-				if (doc != null && !hidden) {
-					exports.push('window["OIMO"]["$className"] = $jsClassName;');
+					var doc = classType.doc;
+					var hidden = classType.meta.extract(":dox").exists((me) -> me.params.exists((e) -> e.expr.equals((macro hide).expr)));
+					var isInterface = classType.meta.has(":interface");
+					if (doc != null && !hidden) {
+						exports.push('window["OIMO"]["$className"] = $jsClassName;');
 
-					var pubMembers = classType.fields.get().filter((cf) -> cf.isPublic && !cf.name.startsWith("_"));
-					var pubStats = classType.statics.get().filter((cf) -> cf.isPublic && !cf.name.startsWith("_"));
-					var pubFields = pubMembers.concat(pubStats);
+						var pubMembers = classType.fields.get().filter((cf) -> cf.isPublic && !cf.name.startsWith("_"));
+						var pubStats = classType.statics.get().filter((cf) -> cf.isPublic && !cf.name.startsWith("_"));
+						var pubFields = pubMembers.concat(pubStats);
 
-					if (isInterface) {
-						pubFields.iter((pf) -> externs.push('OIMO.$className.${pf.name};'));
-					} else {
-						var pubVars = pubFields.filter((cf) -> cf.kind.match(FVar(_, _)));
-						pubVars.iter((pf) -> externs.push('OIMO.$className.${pf.name};'));
+						if (isInterface) {
+							pubFields.iter((pf) -> externs.push('OIMO.$className.${pf.name};'));
+						} else {
+							var pubVars = pubFields.filter((cf) -> cf.kind.match(FVar(_, _)));
+							pubVars.iter((pf) -> externs.push('OIMO.$className.${pf.name};'));
 
-						var pubMethods = pubMembers.filter((cf) -> cf.kind.match(FMethod(_)));
-						var pubFuncs = pubStats.filter((cf) -> cf.kind.match(FMethod(_)));
-						pubMethods.iter((pf) -> exports.push('$jsClassName.prototype["${pf.name}"] = $jsClassName.prototype.${pf.name};'));
-						pubFuncs.iter((pf) -> exports.push('$jsClassName["${pf.name}"] = $jsClassName.${pf.name};'));
+							var pubMethods = pubMembers.filter((cf) -> cf.kind.match(FMethod(_)));
+							var pubFuncs = pubStats.filter((cf) -> cf.kind.match(FMethod(_)));
+							pubMethods.iter((pf) -> exports.push('$jsClassName.prototype["${pf.name}"] = $jsClassName.prototype.${pf.name};'));
+							pubFuncs.iter((pf) -> exports.push('$jsClassName["${pf.name}"] = $jsClassName.${pf.name};'));
+						}
 					}
 				}
-			}
-		case _:
+			case _:
 		});
 		if (WRITE_IMPORTS) {
 			sys.io.File.saveContent("js_imports.txt", imports.join("\n"));
@@ -95,7 +95,6 @@ class B {
 		var fs2:Array<Field> = [];
 		log("filtering... " + fs);
 		for (field in fs) {
-
 			// hide private/internal properties
 			if (field.name.substr(0, 1) == '_') {
 				field.meta.push({
@@ -112,13 +111,14 @@ class B {
 
 					var names:Array<String> = field.name.names(t);
 					if (names != null) {
-
-						U.pushVariables(fs2, names, macro:Float, [{
-							// hide private vars
-							name: ":dox",
-							params: [macro hide],
-							pos: U.pos()
-						}], field.access);
+						U.pushVariables(fs2, names, macro :Float, [
+							{
+								// hide private vars
+								name: ":dox",
+								params: [macro hide],
+								pos: U.pos()
+							}
+						], field.access);
 
 						field.meta.push({
 							name: ":extern",
@@ -135,7 +135,8 @@ class B {
 					log("args " + f.args);
 					for (arg in f.args) {
 						log("arg " + arg);
-						if (arg.type == null) continue;
+						if (arg.type == null)
+							continue;
 						var names:Array<String> = arg.name.names(arg.type);
 						log("names " + names);
 						if (names != null) {
@@ -143,7 +144,7 @@ class B {
 								log("name " + name);
 								args.push({
 									name: name,
-									type: macro:Float
+									type: macro :Float
 								});
 							}
 						} else {
@@ -170,34 +171,30 @@ class B {
 
 	static function filterFuncExpr(e:Expr):Expr {
 		log("filterFuncExpr " + e);
-		switch(e.expr) {
-		case EVars(vars):
-			log("EVars(" + vars + ")");
-			var newVars:Array<Var> = vars.copy();
-			for (v in vars) {
-				log("v " + v);
-				var names:Array<String> = v.name.names(v.type);
-				log("names " + names);
-				if (names != null) {
-					newVars = newVars.concat(names.map(function(name) {
-						return {
-							name: name,
-							type: macro:Float,
-							expr: null,
-							meta: null,
-							isFinal: false
-						};
-					}));
+		switch (e.expr) {
+			case EVars(vars):
+				log("EVars(" + vars + ")");
+				var newVars:Array<Var> = vars.copy();
+				for (v in vars) {
+					log("v " + v);
+					var names:Array<String> = v.name.names(v.type);
+					log("names " + names);
+					if (names != null) {
+						newVars = newVars.concat(names.map(function(name):Var {
+							return {
+								name: name,
+								type: macro :Float,
+								isFinal: false
+							};
+						}));
+					}
 				}
-			}
-			e.expr = EVars(newVars);
-			return e;
-		case _:
-			log("others");
-			return ExprTools.map(e, filterFuncExpr);
+				e.expr = EVars(newVars);
+				return e;
+			case _:
+				log("others");
+				return ExprTools.map(e, filterFuncExpr);
 		}
 	}
-
-#end
-
+	#end
 }
